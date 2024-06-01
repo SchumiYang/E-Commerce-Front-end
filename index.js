4357aaa7cec5920540f9e5a670f7527c01c7a11c
darrenlau <darren.lau.xh@gmail.com>
2024-06-02 01:28:01 +0800

    update ad slider

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let list = document.querySelector('.list');
+    let items = document.querySelectorAll('.slider .list .item');
+    let dots = document.querySelectorAll('.slider .dots li');
+    let prev = document.getElementById('prev');
+    let next = document.getElementById('next');
+
+    let active = 0;
+    let lengthItems = items.length - 1;
+
+    next.onclick = function() {
+        if (active + 1 > lengthItems) {
+            active = 0;
+        } else {
+            active += 1;
+        }
+        reloadSlider();
+    }
+
+    prev.onclick = function() {
+        if (active - 1 < 0) {
+            active = lengthItems;
+        } else {
+            active -= 1;
+        }
+        reloadSlider();
+    }
+
+    function reloadSlider() {
+        let checkLeft = items[active].offsetLeft;
+        list.style.left = -checkLeft + 'px';
+        dots.forEach((dot, index) => {
+            dot.classList.toggle('active', index === active);
+        });
+    }
+});
