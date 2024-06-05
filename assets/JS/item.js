924585b2669cc6f0e92a39307d124f487fa7a670
SchumiYang <schumiy.tw@gmail.com>
2024-06-06 01:23:10 +0800

    a lot update

---
+++ assets/JS/item.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const configItems = document.querySelectorAll('.config-item');
+
+    configItems.forEach(configItem => {
+        const targetClass = configItem.getAttribute('data-target');
+        const targetElem = document.querySelector(`.${targetClass}`);
+
+        configItem.addEventListener('mouseover', function() {
+            if (targetElem) {
+                targetElem.style.borderColor = '#00ffcc';
+            }
+            configItem.classList.add('config-item-hover');
+        });
+
+        configItem.addEventListener('mouseout', function() {
+            if (targetElem) {
+                targetElem.style.borderColor = 'transparent';
+            }
+            configItem.classList.remove('config-item-hover');
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const minusButton = document.querySelector('.quantity .minus');
+    const plusButton = document.querySelector('.quantity .plus');
+    const inputField = document.querySelector('.quantity input');
+
+    minusButton.addEventListener('click', function() {
+        let currentValue = parseInt(inputField.value);
+        if (currentValue > 1) {
+            inputField.value = currentValue - 1;
+        } else {
+            alert("At least 1")
+        }
+    });
+
+    plusButton.addEventListener('click', function() {
+        let currentValue = parseInt(inputField.value);
+        inputField.value = currentValue + 1;
+    });
+});
